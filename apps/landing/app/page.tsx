56556b76b2f8ed74ab53105df008c70a028f4787
Hamid Yeganeh <hamidyeganeh82@gmail.com>
2025-11-19 14:15:19 +0330

    [services][add] artworks & auth apis

--- apps/landing/app/page.tsx
+++ apps/landing/app/page.tsx
@@ -1,10 +1,9 @@
-"use client";
 import { Button } from "@paletto/ui";
-import { ApiClient } from "@paletto/apis";
 import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
 
-export default function Page() {
-  const t = useTranslations();
+export default async function Page() {
+  const t = await getTranslations({ locale: "en" });
   return (
     <div className="p-6 space-y-4">
       <h1 className="text-2xl font-bold bg-primary-500">{t("hello")}</h1>
